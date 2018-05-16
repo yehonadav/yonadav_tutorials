e0b8a7a7bb73122756338d856aa2b736fd5aae28
yonadav <relin.studio@gmail.com>
2018-05-16 20:44:58 +0300

    desktop automation

--- learn_python/desktop_automation/open_pptx.py
+++ learn_python/desktop_automation/open_pptx.py
@@ -17,20 +17,24 @@ class Desktop:
         def __init__(self):
             self.current_slide = 0
 
-        def open(self, file='slide2.pptx'):
+        def open(self, file='slide2.pptx', wait=10):
             os.startfile(file)
             self.current_slide = 1
-            time.sleep(10)
+            time.sleep(wait)
 
         @staticmethod
         def start_presentation_mode():
-            keyboard.press_and_release('f5')
+            keyboard.press_and_release('shift+f5')
             time.sleep(1)
 
         @staticmethod
         def exit_presentation_mode():
             keyboard.press_and_release('esc')
             time.sleep(1)
+
+        def go_to_slide(self, number):
+            keyboard.press_and_release('{}+enter'.format(number))
+            self.current_slide = number
 
         def next_slide(self, wait=0):
             keyboard.press_and_release('page down')
@@ -52,12 +56,13 @@ if __name__ == "__main__":
     pptx = Desktop.PPTX()
     pptx.open()
     pptx.start_presentation_mode()
-    pptx.next_slide(wait=2)
-    pptx.next_slide(wait=2)
-    pptx.next_slide(wait=2)
-    pptx.previous_slide(wait=2)
-    pptx.previous_slide(wait=2)
-    pptx.previous_slide(wait=2)
+    # pptx.next_slide(wait=2)
+    # pptx.next_slide(wait=2)
+    # pptx.next_slide(wait=2)
+    # pptx.previous_slide(wait=2)
+    # pptx.previous_slide(wait=2)
+    # pptx.previous_slide(wait=2)
+    pptx.go_to_slide(2)
     pptx.exit_presentation_mode()
     pptx.close()
-    Desktop.switch_window(5)
+    # Desktop.switch_window(5)

46e8fe817b249ccb4238d2e09e406a2f4baf8285
yonadav <relin.studio@gmail.com>
2018-05-16 17:29:12 +0300

    desktop automation

--- learn_python/desktop_automation/open_pptx.py
+++ learn_python/desktop_automation/open_pptx.py
@@ -4,39 +4,53 @@ import mouse
 import time
 
 
-class PPTX:
-    def __init__(self):
-        self.current_slide = 0
+class Desktop:
 
     @staticmethod
-    def open_pptx(file='slide2.pptx'):
-        os.startfile(file)
-        time.sleep(10)
+    def switch_window(times=1):
+        keyboard.press('alt')
+        for _ in range(times):
+            keyboard.press_and_release('tab')
+        keyboard.release('alt')
 
-    @staticmethod
-    def start_presentation_mode():
-        keyboard.press_and_release('f5')
-        time.sleep(1)
+    class PPTX:
+        def __init__(self):
+            self.current_slide = 0
 
-    @staticmethod
-    def exit_presentation_mode():
-        keyboard.press_and_release('esc')
-        time.sleep(1)
+        def open(self, file='slide2.pptx'):
+            os.startfile(file)
+            self.current_slide = 1
+            time.sleep(10)
 
-    def next_slide(self, wait=0):
-        keyboard.press_and_release('page down')
-        self.current_slide += 1
-        time.sleep(wait)
+        @staticmethod
+        def start_presentation_mode():
+            keyboard.press_and_release('f5')
+            time.sleep(1)
 
-    def previous_slide(self, wait=0):
-        keyboard.press_and_release('page up')
-        self.current_slide -= 1
-        time.sleep(wait)
+        @staticmethod
+        def exit_presentation_mode():
+            keyboard.press_and_release('esc')
+            time.sleep(1)
+
+        def next_slide(self, wait=0):
+            keyboard.press_and_release('page down')
+            self.current_slide += 1
+            time.sleep(wait)
+
+        def previous_slide(self, wait=0):
+            keyboard.press_and_release('page up')
+            self.current_slide -= 1
+            time.sleep(wait)
+
+        def close(self):
+            keyboard.press_and_release('alt+f4')
+            self.current_slide = 0
+            time.sleep(1)
 
 
 if __name__ == "__main__":
-    pptx = PPTX()
-    pptx.open_pptx()
+    pptx = Desktop.PPTX()
+    pptx.open()
     pptx.start_presentation_mode()
     pptx.next_slide(wait=2)
     pptx.next_slide(wait=2)
@@ -45,3 +59,5 @@ if __name__ == "__main__":
     pptx.previous_slide(wait=2)
     pptx.previous_slide(wait=2)
     pptx.exit_presentation_mode()
+    pptx.close()
+    Desktop.switch_window(5)

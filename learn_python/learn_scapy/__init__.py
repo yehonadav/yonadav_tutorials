ed772a22191dcec8e2d923a20a90295b37d24e1d
qaviton <qaviton@gmail.com>
2018-10-10 20:33:40 +0300

    scapy

---
+++ learn_python/learn_scapy/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""download this for windows: https://www.winpcap.org/install/
+"""
+
+from scapy.all import *
+
+
+# sniff(prn=lambda x: x.show(), filter="host x.x.x.x", store=0)
+
+
+stars = lambda n: "*" * n
+
+def GET_print(packet):
+    return "\n".join((
+        stars(40) + "GET PACKET" + stars(40),
+        "\n".join(packet.sprintf("{Raw:%Raw.load%}").split(r"\r\n")),
+        stars(90)))
+
+sniff(prn=GET_print, filter="host x.x.x.x", store=0)
